cbd8c640441476aa0b7e6d6e820fa47521ead90b
Priyail <nidhipandey8108@gmail.com>
2026-04-09 07:45:44 +0530

    fix production API URL

--- frontend/script.js
+++ frontend/script.js
@@ -37,6 +37,7 @@ document.addEventListener('DOMContentLoaded', () => {
         btnText.textContent = "Fetching Case Details...";
         submitBtn.disabled = true;
         // fetch('http://127.0.0.1:5000/search_case') for local testing
+        // fetch('https://court-data-fetcher-mini-dashboard-3ym9.onrender.com/search_case') for deployed backend
         try {
             const response = await fetch('https://court-data-fetcher-mini-dashboard-3ym9.onrender.com/search_case', {
                 method: 'POST',
